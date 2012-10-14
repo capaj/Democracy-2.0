a23affb0502864ecf8a558d548d4b620ba2a40fa
Jiri Spac <capajj@gmail.com>
2012-10-14 23:40:19 +0200

    +modal warning for other clients
    we are only targeting chrome right now

--- Dem2Client/main.js
+++ Dem2Client/main.js
@@ -1,9 +1,20 @@
 ﻿
 IS_RUNNING_ON_SERVER = true;    //use true when deploying on the live server
-IS_RUNNING_ON_SERVER = false;   // use false value for local testing
+//IS_RUNNING_ON_SERVER = false;   // use false value for local testing
+
+navigator.sayswho = (function () {      // thanks to kennebec on stackoverflow.com
+    var N = navigator.appName, ua = navigator.userAgent, tem;
+    var M = ua.match(/(opera|chrome|safari|firefox|msie)\/?\s*(\.?\d+(\.\d+)*)/i);
+    if (M && (tem = ua.match(/version\/([\.\d]+)/i)) != null) M[2] = tem[1];
+    M = M ? [M[1], M[2]] : [N, navigator.appVersion, '-?'];
+    return M;
+})();
 
 require(["Scripts/facebook"], function (FB) {
- 
+    if (navigator.sayswho[0] != "Chrome") {
+        $('#notChromeWarning').modal('show')
+    }
+
     var WSworker = new Worker('Scripts/wsworker.js');   //worker handling server comunication
     WSworker.onmessage = function (event) {
         switch (event.data.type) {
